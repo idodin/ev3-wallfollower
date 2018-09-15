2600f42ac127cbbe2a30429c07b79683d083168a
Imad Dodin <imad.dodin@mail.mcgill.ca>
2018-09-15 11:26:34 -0400

    Add Provided Code Files

---
+++ src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
@@ -0,0 +1,35 @@
+package ca.mcgill.ecse211.wallfollowing;
+
+import lejos.hardware.motor.*;
+
+public class BangBangController implements UltrasonicController {
+
+  private final int bandCenter;
+  private final int bandwidth;
+  private final int motorLow;
+  private final int motorHigh;
+  private int distance;
+
+  public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
+    // Default Constructor
+    this.bandCenter = bandCenter;
+    this.bandwidth = bandwidth;
+    this.motorLow = motorLow;
+    this.motorHigh = motorHigh;
+    WallFollowingLab.leftMotor.setSpeed(motorHigh); // Start robot moving forward
+    WallFollowingLab.rightMotor.setSpeed(motorHigh);
+    WallFollowingLab.leftMotor.forward();
+    WallFollowingLab.rightMotor.forward();
+  }
+
+  @Override
+  public void processUSData(int distance) {
+    this.distance = distance;
+    // TODO: process a movement based on the us distance passed in (BANG-BANG style)
+  }
+
+  @Override
+  public int readUSDistance() {
+    return this.distance;
+  }
+}
