64efef3825c4efe0d1751d2f874c00ee6b75c1c5
Imad Dodin <imad.dodin@mail.mcgill.ca>
2018-09-15 13:45:22 -0400

    Java SDK 1.7

--- src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
+++ src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
@@ -9,6 +9,7 @@ public class BangBangController implements UltrasonicController {
   private final int motorLow;
   private final int motorHigh;
   private int distance;
+  private int distError;
 
   public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
     // Default Constructor
@@ -25,7 +26,27 @@ public class BangBangController implements UltrasonicController {
   @Override
   public void processUSData(int distance) {
     this.distance = distance;
-    // TODO: process a movement based on the us distance passed in (BANG-BANG style)
+    distError = bandCenter - distance;
+    
+    if (Math.abs(distError) <= bandwidth) {
+    	WallFollowingLab.leftMotor.setSpeed(motorHigh);
+    	WallFollowingLab.rightMotor.setSpeed(motorHigh);
+        WallFollowingLab.leftMotor.forward();
+        WallFollowingLab.rightMotor.forward();
+    }
+    else if (distError > 0) {
+    	WallFollowingLab.leftMotor.setSpeed(motorLow);
+    	WallFollowingLab.rightMotor.setSpeed(motorHigh);
+        WallFollowingLab.leftMotor.forward();
+        WallFollowingLab.rightMotor.forward();
+    }
+    else if (distError < 0) {
+    	WallFollowingLab.leftMotor.setSpeed(motorHigh);
+    	WallFollowingLab.rightMotor.setSpeed(motorLow);
+        WallFollowingLab.leftMotor.forward();
+        WallFollowingLab.rightMotor.forward();
+    }
+    
   }
 
   @Override
